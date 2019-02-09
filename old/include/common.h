62c96662165b72a747ad23cc18c34b64ba416af9
qingyiwebt <3211468886@qq.com>
2019-02-10 00:19:25 +0800

    清理:没用的东西

--- old/include/common.h
+++
@@ -1,6 +0,0 @@
-#ifndef inced_common_
-#define inced_common_
-
-void kitoa(int num, char *str, unsigned int radix);
-
-#endif
--- old/include/system.h
+++
@@ -1,8 +0,0 @@
-#ifndef inced_system_
-#define inced_system_
-
-void cleanf();
-void _printf(const char c);
-void printf(const char* str);
-void asm_hlt();
-#endif
