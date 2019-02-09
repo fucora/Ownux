1addf42b837e8fa4b645a5a903ce88d182ce668b
qingyiwebt <3211468886@qq.com>
2019-02-10 00:57:03 +0800

    添加:types.h

---
+++ include/types.h
@@ -0,0 +1,11 @@
+#ifndef Inced_types_h
+#define Inced_types_h
+
+typedef unsigned int   uint32_t;
+typedef          int   int32_t;
+typedef unsigned short uint16_t;
+typedef          short int16_t;
+typedef unsigned char  uint8_t;
+typedef          char  int8_t;
+
+#endif
