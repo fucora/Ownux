7adf9566e40f2b4c5fc5e527897cb8ec7f542fa4
qingyiwebt <3211468886@qq.com>
2019-02-10 00:08:51 +0800

    添加:用于在引导区的代码

---
+++ src/boot.asm
@@ -0,0 +1,36 @@
+;==GRUB MB的设定
+MBOOT_HEADER_MAGIC 	equ 	0x1BADB002
+MBOOT_PAGE_ALIGN 	equ 	1 << 0 
+MBOOT_HEADER_FLAGS 	equ 	MBOOT_PAGE_ALIGN | MBOOT_MEM_INFO
+MBOOT_CHECKSUM 		equ 	- (MBOOT_HEADER_MAGIC + MBOOT_HEADER_FLAGS)
+;==代码的开始
+[BITS 32]
+section .text
+;======MB的标记
+dd MBOOT_HEADER_MAGIC
+dd MBOOT_HEADER_FLAGS
+dd MBOOT_CHECKSUM
+;======GLOBAL
+[GLOBAL start]
+[GLOBAL glb_mboot_ptr]
+;======这鬼函数让链接器自己找去!
+[EXTERN cmain]
+;======start函数
+start:
+    cli
+    mov esp,STACK_TOP
+    mov ebp,0
+    and esp,0FFFFFFF0H
+    mov [glb_mboot_ptr],ebx
+    call cmain
+stop:
+    ;CPU休眠
+    hlt
+    jmp stop
+section .bss
+stack:
+	resb 32768
+glb_mboot_ptr: 	
+	resb 4
+
+STACK_TOP equ $-stack-1
---
+++ src/kernel.c
@@ -0,0 +1,3 @@
+int cmain(){
+    return 0;
+}
