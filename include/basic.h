ff63a6fbe006800fffc6708b2e0db4e7333298b7
qingyiwebt <3211468886@qq.com>
2019-02-11 00:45:55 +0800

    添加

---
+++ include/basic.h
@@ -0,0 +1,13 @@
+#ifndef Inced_basic_h
+#define Inced_basic_h
+
+#ifndef NULL
+#define NULL 0
+#endif
+
+#ifndef TRUE
+#define FALSE 0
+#define TRUE 1
+#endif
+
+#endif
---
+++ include/debug.h
@@ -0,0 +1,25 @@
+#ifndef Inced_debug_h
+#define Inced_debug_h
+
+#include "console.h"
+#include "vargs.h"
+#include "elf.h"
+
+#define assert(x, info)                                       	\
+	do {                                                	\
+		if (!(x)) {                                     \
+			panic(info); 		     		\
+		}                                               \
+	} while (0)
+
+#define static_assert(x)                                	\
+	switch (x) { case 0: case (x): ; }
+
+
+void init_debug();
+void panic(const char *msg);
+void print_cur_status();
+void printk(const char *format, ...);
+void printk_color(real_color_t back, real_color_t fore, const char *format, ...);
+
+#endif
